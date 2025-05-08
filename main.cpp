b90a2d552bf001329cb50f7419aa39b2e9f45516
Water Chika <water_chika@outlook.com>
2025-05-08 16:03:43 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿#include <cpp_helper.hpp>
+#include <utility>
+#include <cassert>
+
+int main() {
+    auto fun = cpp_helper::overloads{
+        [](auto x){
+            return x;
+        },
+        [](auto x, auto y) {
+            return std::pair{x, y};
+        }
+    };
+
+    auto [x, y] = fun(1, 3.0f);
+    assert(x == 1 && y == 3.0f);
+    return 0;
+}

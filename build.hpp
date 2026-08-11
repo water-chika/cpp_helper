d844b9a92a1787a7c0d625004b0ebbf3fa1a5b92
Water Chika <water_chika@outlook.com>
2026-08-11 21:18:37 +0800

    update

--- build.hpp
+++ build.hpp
@@ -2,8 +2,8 @@
 
 namespace cpp_helper_build {
 
-void add(build::builder& builder, std::filesystem::path path) {
-    builder.add_library("cpp_helper", absolute(path).string());
+void add(build::builder& builder, std::filesystem::path path, std::filesystem::path build_dir) {
+    builder.add_library("cpp_helper", build_dir / "cpp_helper", absolute(path).string());
 }
 
 }
